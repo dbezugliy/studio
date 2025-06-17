95bc7fc92acf0d4b80cae9745db78faf51d95a41
dbezugliy <dbezugliy@imsa.edu>
2025-06-16 20:10:07 -0500

    challenge5

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ class ChallengeCarousel {
             "This was tough. Was trying recreate a water droplet effect that would displace a video properly as done in a @RC_REJECT promotion, but I couldn't get it quite right. Especially, the water drip at the top looks bad, but I ran out of time to keep working. I imagine they didn't use After Effects for this shot. Will have to revisit this one.",
             "Messed around with simple text pop-in and icon animations. A lot of videos try and grab the viewer's attention this way, and I think @maxklymenko does it well. Of course it's more interesting to try to recreate more complicated effects, but optimizing the basics is also important.",
             "Point clouds. I just wanted to work with some point clouds. They have always been on my mind but I never implemented it in editing. So, I took a video I shot at an art exibit, mixed it with some gaussian splatting, and voilà.",
-            ""
+            "The style used in the artwork from @thatskidding on the right is really good, so I tried to 3D model the charecter figure in Blender. In the future, I want to learn more about animation techniques in Blender than actual modeling."
         ];
 
 
